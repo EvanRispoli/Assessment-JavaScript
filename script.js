de6a994c63a7851e57cfcae51eac5fb8313c8fc4
Evan Rispoli <evan.rispoli@gmail.com>
2022-04-15 14:22:06 -0300

    feat: :sparkles: Cria colunas e imagens
    Dentro de cada Div "row" criamos 4 divs com class= "col-3" e dentro de cada "col-3" criamos uma imagem que sao as cartas do jogo

--- script.js
+++ script.js
@@ -7,21 +7,40 @@
     imagemEspecifica : "https://picsum.photos/id/",
     fundo : `https://picsum.photos/id/125/80?grayscale`,
 
-    criaLinhas : function(){
+    criaTabuleiro : function(){
+      //Cria 4 divs com classe row
       for (let i = 0; i < 4; i++) {
         let linha = document.createElement("div");
         linha.className = "row";
         this.tabuleiro.appendChild(linha);
+        // Cria 4 divs com classe Col-3 dentro de cada uma das rows
+        for (let i = 0; i < 4 ; i++){
+          let coluna = document.createElement("div")
+          coluna.className = ("col-3")
+          linha.appendChild(coluna)
+          // Cria uma imagem para cada coluna e atribui class= carta e id= vazio
+        let imagem = document.createElement("img");
+        imagem.id = "";
+        imagem.className = "carta";
+        imagem.src = this.fundo;
+        imagem.style.opacity = "0.5";
+        coluna.appendChild(imagem);
+        }
       }
+
     },
+
+    criaColunas : function(){
+      
+    }
 
 
     
   }
   onload = () => {
     
-    app.criaLinhas();
-    app.criaColunas()
+    app.criaTabuleiro();
+    //app.criaColunas()
     
   }
 })();
