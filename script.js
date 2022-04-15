466c8a46873838175a266a1e83b8dc04590de56a
Evan Rispoli <evan.rispoli@gmail.com>
2022-04-15 16:07:42 -0400

    Update script.js
    * Funcao reponsavel pela criacao de 4 colunas (div class="col-3") para cada linha (div class="row")
    * * Funcao reponsavel pela criacao de uma imagem para cada coluna (div class="col-3")

--- script.js
+++ script.js
@@ -6,22 +6,47 @@
     lista :"https://picsum.photos/v2/list?page=2&limit=8",
     imagemEspecifica : "https://picsum.photos/id/",
     fundo : `https://picsum.photos/id/125/80?grayscale`,
-
+	
+	
+// Cria 4 divs com class= "row"
     criaLinhas : function(){
       for (let i = 0; i < 4; i++) {
         let linha = document.createElement("div");
         linha.className = "row";
         this.tabuleiro.appendChild(linha);
-      }
+      }  
+	  this.criaColunas();
     },
-
-
+// Cria 4 divs com class="col-3" para cada uma das divs com class = "row"	
+	criaColunas : function() {
+		var linhas = document.querySelectorAll(".row");
+		console.log(linhas[0]);
+		for(let i=0 ; i < linhas.length; i++){
+			for(let j= 0; j < 4; j++){
+				let coluna = document.createElement("div");
+			coluna.className = "col-3";
+			linhas[i].appendChild(coluna);
+			}
+		}
+		this.criaImagens();
+	},
+/* Cria 1 imagem para cada div class="col-3". As imagens possuem class="carta" 
+e para cada uma delas foi atribuida um data-valor=0. */	
+	criaImagens : function() {
+		var colunas = document.querySelectorAll(".col-3");
+		for(let i = 0 ; i < colunas.length; i++){
+			let imagem = document.createElement("img");
+			imagem.className = "carta";
+			imagem.src = this.fundo;
+			imagem.setAttribute("data-valor",0);
+			colunas[i].appendChild(imagem);
+		}
+	},
     
   }
   onload = () => {
-    
     app.criaLinhas();
-    app.criaColunas()
+    
     
   }
 })();
