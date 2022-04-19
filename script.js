90c18b8d17eadacc9d64e956c8a51e3626c62208
Evan Rispoli <evan.rispoli@gmail.com>
2022-04-19 01:10:40 -0300

    Criacao de verifica combinacao

--- script.js
+++ script.js
@@ -1,134 +1,174 @@
 (() => {
   let app = {
-    //Criando atributos pra facilitar a codificação
-    tabuleiro: document.querySelector("#tabuleiro"),
-    lista: "https://picsum.photos/v2/list?page=2&limit=8",
-    imagemEspecifica: "https://picsum.photos/id/",
-    fundo: `https://picsum.photos/id/125/80?grayscale`,
-    tamanhoImagem: 80,
-    imagensIds: [],
+    iniciaJogo: function() {
+      //Criando atributos pra facilitar a codificação
 
-    iniciaJogo: function(){
-      this.criaLinhas();
-    },
+      let tabuleiro = document.querySelector("#tabuleiro");
+      let lista = "https://picsum.photos/v2/list?page=2&limit=8";
+      let imagemEspecifica = "https://picsum.photos/id/";
+      let fundo = `https://picsum.photos/id/125/80?grayscale`;
+      let tamanhoImagem = 80;
+      let imagensId = [];
 
-    // Cria 4 divs com class= "row"
-    criaLinhas: function() {
-      console.log("inicio cria linhas");
-      for (let i = 0; i < 4; i++) {
-        let linha = document.createElement("div");
-        linha.className = "row";
-        this.tabuleiro.appendChild(linha);
-      }
-      this.criaColunas();
-    },
-
-    // Cria 4 divs com class="col-3" para cada uma das divs com class = "row"
-    criaColunas: function() {
-      console.log("inicio cria colunas");
-      var linhas = document.querySelectorAll(".row");
-      for (let i = 0; i < linhas.length; i++) {
-        for (let j = 0; j < 4; j++) {
-          let coluna = document.createElement("div");
-          coluna.className = "col-3";
-          linhas[i].appendChild(coluna);
+      criaLinhas();
+      // Cria 4 divs com class= "row"
+      function criaLinhas() {
+        console.log("inicio cria linhas");
+        for (let i = 0; i < 4; i++) {
+          let linha = document.createElement("div");
+          linha.className = "row";
+          tabuleiro.appendChild(linha);
         }
+        criaColunas();
       }
-      this.criaImagens();
-    },
 
-    /* Cria 1 imagem para cada div class="col-3". As imagens possuem class="carta" e para cada uma delas foi atribuída um data-valor=0. */
-
-    criaImagens: function() {
-      console.log("inicio cria imagens");
-      var colunas = document.querySelectorAll(".col-3");
-      for (let i = 0; i < colunas.length; i++) {
-        let imagem = document.createElement("img");
-        imagem.className = "carta";
-        imagem.src = this.fundo;
-        
-        imagem.crossorigin = "anonymous";
-        colunas[i].appendChild(imagem);
-      }
-      this.buscaImagens();
-    },
-
-    /*Faz a busca das imagens na api da PicSum e armazena os id's das imagens 2 vezes em uma lista*/
-    buscaImagens: function() {
-      console.log("inicio busca imagens");
-      fetch(this.lista).then(resposta => resposta.json()).then(listaImagens => {
-        for (imagem of listaImagens) {
-          for (let i = 0; i < 2; i++) {
-            this.imagensIds.push(imagem.id);
+      // Cria 4 divs com class="col-3" para cada uma das divs com class = "row"
+      function criaColunas() {
+        console.log("inicio cria colunas");
+        var linhas = document.querySelectorAll(".row");
+        for (let i = 0; i < linhas.length; i++) {
+          for (let j = 0; j < 4; j++) {
+            let coluna = document.createElement("div");
+            coluna.className = "col-3";
+            linhas[i].appendChild(coluna);
           }
         }
-        console.log("fim fetch");
-        this.embaralhaCartas();
-      });
-    },
-
-    //Embaralha os endereços na lista imagensId
-    embaralhaCartas: function() {
-      console.log("inicio embaralhamento");
-      for (let i = 0; i < this.imagensIds.length; i++) {
-        let p = Math.floor(Math.random() * this.imagensIds.length);
-        let aux = this.imagensIds[p];
-        this.imagensIds[p] = this.imagensIds[i];
-        this.imagensIds[i] = aux;
+        criaImagens();
       }
-      this.viraCartas();
-    },
 
-    /*Muda as src`s das imagens de acordo com a lista de Ids e atribui o id como valor de 
-    referencia para a carta*/
-    viraCartas: function() {
-      
-      var imagens = document.querySelectorAll("#tabuleiro img");
-      for (let i = 0; i < imagens.length; i++) {
-        imagens[i].classList.add('virada')
-        imagens[i].setAttribute("data-valor", this.imagensIds[i]);
-        imagens[i].src = `${this.imagemEspecifica}${this.imagensIds[i]}/${this
-          .tamanhoImagem}`;
+      /* Cria 1 imagem para cada div class="col-3". As imagens possuem class="carta" e para cada uma delas foi atribuída um data-valor=0. */
+      function criaImagens() {
+        console.log("inicio cria imagens");
+        var colunas = document.querySelectorAll(".col-3");
+        for (let i = 0; i < colunas.length; i++) {
+          let imagem = document.createElement("img");
+          imagem.className = "carta";
+          imagem.src = fundo;
+
+          imagem.crossorigin = "anonymous";
+          colunas[i].appendChild(imagem);
+        }
+        buscaImagens();
       }
-      setTimeout(() => {
-        this.desviraCartas();
-      }, 5000);
-    },
-    // Retorna as src`s das imagens para a imagem de fundo
-    desviraCartas: function() {
-      console.log("inicio desvira cartas");
-      var imagens = document.querySelectorAll("#tabuleiro img");
 
-      for (let i = 0; i < imagens.length; i++) {
-        imagens[i].classList.remove('virada')
-        imagens[i].src = `${this.fundo}`;
-        imagens[i].crossorigin = "anonymous";
+      /*Faz a busca das imagens na api da PicSum e armazena os id's das imagens 2 vezes em uma lista*/
+      async function buscaImagens() {
+        console.log("inicio busca imagens");
+        await fetch(lista)
+          .then(resposta => resposta.json())
+          .then(listaImagens => {
+            for (imagem of listaImagens) {
+              for (let i = 0; i < 2; i++) {
+                imagensId.push(imagem.id);
+              }
+            }
+            console.log("fim fetch");
+            embaralhaCartas();
+          });
       }
-    },
+      //Embaralha os endereços na lista imagensId
+      function embaralhaCartas() {
+        console.log("inicio embaralhamento");
+        for (let i = 0; i < imagensId.length; i++) {
+          let p = Math.floor(Math.random() * imagensId.length);
+          let aux = imagensId[p];
+          imagensId[p] = imagensId[i];
+          imagensId[i] = aux;
+        }
+        viraCartas();
+      }
+      /*Muda as src`s das imagens de acordo com a lista de Ids e atribui o id como valor de 
+      referencia para a carta*/
+      function viraCartas() {
+        console.log("Vira Cartas");
+        let imagens = document.querySelectorAll("#tabuleiro img");
+        for (let i = 0; i < imagens.length; i++) {
+          imagens[i].classList.add("virada");
+          imagens[i].setAttribute("data-valor", imagensId[i]);
+          imagens[i].src = `${imagemEspecifica}${imagensId[
+            i
+          ]}/${tamanhoImagem}`;
+        }
+        setTimeout(() => {
+          desviraCartas();
+        }, 5000);
+      }
+      // Retorna as src`s das imagens para a imagem de fundo
+      function desviraCartas() {
+        console.log("inicio desvira cartas");
+        var imagens = document.querySelectorAll("#tabuleiro img");
 
-    //Variáveis auxiliares para controlar comportamento do jogo
-    cliqueTravado: false,
-    temCartaVirada: false,
-    posicaoCartaVirada: -1,
-    pontos: 0,
-    primeiraCarta: null,
-    segundaCarta: null,
+        for (let i = 0; i < imagens.length; i++) {
+          imagens[i].classList.remove("virada");
+          imagens[i].src = `${fundo}`;
+          imagens[i].crossorigin = "anonymous";
+        }
+      }
 
-    resetaEstadosJogo: function(){
-      [this.cliqueTravado, this.temCartaVirada] = [false, false];
-      [this.primeiraCarta, this.segundaCarta] = [null, null];
-    },
+      //Variáveis auxiliares para controlar comportamento do jogo
+      const cartas = document.querySelectorAll(".col-3 img");
+      let cliqueTravado = false;
+      let temCartaVirada = false;
+      let posicaoCartaVirada = -1;
+      let pontos = 0;
+      let primeiraCarta = null;
+      let segundaCarta = null;
+      let valorCartaVirada = 0;
+      // Vira uma carta
+      function viraCarta() {
+        if (cliqueTravado) return;
+        if (this === primeiraCarta) return;
 
+        this.classList.add("virada");
+        this.src = `${imagemEspecifica}${this.getAttribute(
+          "data-valor"
+        )}/${tamanhoImagem}`;
+        if (!temCartaVirada) {
+          //primeiro click
+          temCartaVirada = true;
+          primeiraCarta = this;
+          console.log(primeiraCarta);
+          return;
+        }
+        // Segundo click
+        segundaCarta= this
 
+        verificaCombinacao();      
+      }
+      function verificaCombinacao(){
+        let combina = primeiraCarta.dataset.valor === segundaCarta.dataset.valor;
+        combina ? desabilitaCartas() : desviraCarta();
+      }
 
+      function desabilitaCartas() {
+        primeiraCarta.removeEventListener('click', viraCarta);
+        segundaCarta.removeEventListener('click', viraCarta);
+      }
 
-     
+      function desviraCarta(){
+        cliqueTravado = true;
 
+        setTimeout(()=>{
+          primeiraCarta.src = `${imagemEspecifica}${primeiraCarta.getAttribute("data-valor")}/${tamanhoImagem}`;
+          primeiraCarta.classList.remove('virada');
+          
+          segundaCarta.classList.remove('virada');
 
+          resetaTabuleiro();
+        },2000)
+      }
+      function resetaTabuleiro(){
+        [temCartaVirada, cliqueTravado] = [false, false];
+  [primeiraCarta, segundaCarta] = [null, null];
+      }
+
+      cartas.forEach(carta => carta.addEventListener("click", viraCarta));
+    }
   };
-  onload = () => {
-    app.criaLinhas();
-    document.querySelector("#btInicio").onclick= app.viraCartas();
+  
 
+  onload = () => {
+    //app.iniciaJogo();
+    document.querySelector("#btInicio").onclick = app.iniciaJogo;
   };
 })();
